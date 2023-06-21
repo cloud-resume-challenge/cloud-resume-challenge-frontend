db092f75acb49c3e66ef039377098aae4c7fa625
austinbrees <austinbrees@gmail.com>
2023-06-21 16:23:26 +0200

    added tests

--- files/app.js
+++ files/app.js
@@ -1,16 +1,21 @@
-window.onload = function() {
+window.onload = async function() {
+    // Get cached count and display it
     var cachedCount = localStorage.getItem('viewCount');
     if (cachedCount) {
         document.getElementById('viewCount').textContent = "Total page views " + cachedCount;
     }
 
-    fetch('https://34.36.31.130.nip.io/backend-counter-api/update-counter', { method: 'POST' })
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('viewCount').textContent = "Total page views " + data.count;
-            localStorage.setItem('viewCount', data.count);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+    // Delay the update function until after onload has completed
+    setTimeout(updateCount, 0);
+}
+
+async function updateCount() {
+    try {
+        let response = await fetch('https://34.36.31.130.nip.io/backend-counter-api/update-counter', { method: 'POST' });
+        let data = await response.json();
+        document.getElementById('viewCount').textContent = "Total page views " + data.count;
+        localStorage.setItem('viewCount', data.count);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
